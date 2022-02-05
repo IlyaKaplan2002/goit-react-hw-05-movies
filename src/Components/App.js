e846a369cd37677705f7b19348b097cc9623dca4
IlyaKaplan2002 <kaplan11092002@gmail.com>
2022-02-05 15:31:02 +0200

    warning fixed

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useState } from 'react';
 import Header from './Header';
 import Main from './Main';
 
